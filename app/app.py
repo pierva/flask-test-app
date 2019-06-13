54bf7b8c324ce3e7269aea6fbd08807063426dc5
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-06-13 13:43:03 -0400

    Created basic flask test app

---
+++ app/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+from flask import Flask, url_for, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/home')
+def showHome():
+    return 'Hello home'
+
+
+@app.route('/about')
+def showAbout():
+    return render_template('about.html')
+
+
+@app.route('/login')
+def showLogin():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run()
